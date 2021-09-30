ac0bc1e08b6d85362a2263e0068e66d46263b6a1
EG-easy <1018eguchi@gmail.com>
2021-09-30 18:35:45 +0900

    add scripts

---
+++ scripts/export.sh
@@ -0,0 +1,4 @@
+nibirud export-airdrop-snapshot uatom ~/mainnet/cosmoshub4-2021-02-18.json ~/02-18-snap.json
+
+nibirud export-airdrop-snapshot uatom ~/mainnet/cosmoshub4-2021-09-27.json ~/09-27-snap.json
+
---
+++ scripts/import.sh
@@ -0,0 +1,1 @@
+nibirud import-genesis-accounts-from-snapshot ~/02-18-snap.json ~/nibiru/game.json
