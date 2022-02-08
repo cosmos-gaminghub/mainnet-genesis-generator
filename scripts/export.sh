92b14dcfd2e811167aa6e61b0a9b5c4975ddae1f
EG-easy <1018eguchi@gmail.com>
2022-02-09 05:11:15 +0900

    update scripts

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,1 +1,1 @@
-nibirud export-airdrop-snapshot uatom ~/mainnet/cosmoshub4-2021-02-18.json ~/mainnet/cosmoshub4-2021-11-01.json ~/snapshot.json
+nibirud export-airdrop-snapshot uatom cosmoshub4-2021-02-18.json cosmoshub4-2021-11-01.json snapshot.json
