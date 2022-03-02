b14602d883ab3b17062d7161d5315e68a10163c3
EG-easy <1018eguchi@gmail.com>
2022-03-02 13:22:51 +0900

    update pre genesis script

--- scripts/create-pre-genesis.sh
+++ scripts/create-pre-genesis.sh
@@ -17,16 +17,14 @@ sed -i -e 's/"min_signed_per_window": "0.500000000000000000"/"min_signed_per_win
 sed -i -e 's/"slash_fraction_downtime": "0.010000000000000000"/"slash_fraction_downtime": "0.000100000000000000"/' $GENESIS
 sed -i -e 's/"voting_period": "172800s"/"voting_period": "1209600s"/' $GENESIS
 sed -i -e 's/"max_deposit_period": "172800s"/"max_deposit_period": "1209600s"/' $GENESIS
-sed -i -e 's/"inflation": "0.130000000000000000"/"inflation": "1.0"/' $GENESIS
-sed -i -e 's/"inflation_rate_change": "0.130000000000000000"/"inflation_rate_change": "1.0"/' $GENESIS
-sed -i -e 's/"inflation_max": "0.200000000000000000"/"inflation_max": "1.0"/' $GENESIS
-sed -i -e 's/"inflation_min": "0.070000000000000000"/"inflation_min": "0.125000000000000000"/' $GENESIS
-sed -i -e 's/"goal_bonded": "0.670000000000000000"/"goal_bonded": "0.340000000000000000"/' $GENESIS
+sed -i -e 's/"inflation": "0.130000000000000000"/"inflation": "0.300000000000000000"/' $GENESIS
+sed -i -e 's/"inflation_max": "0.200000000000000000"/"inflation_max": "0.300000000000000000"/' $GENESIS
+sed -i -e 's/"inflation_min": "0.070000000000000000"/"inflation_min": "0.300000000000000000"/' $GENESIS
 sed -i -e 's/"send_enabled": true/"send_enabled": false/' $GENESIS
 sed -i -e 's/"receive_enabled": true/"receive_enabled": false/' $GENESIS
 
 jq '.app_state.gov.deposit_params.min_deposit[0].amount="1000000000"' $GENESIS > tmp.json && mv tmp.json $GENESIS
-jq '.genesis_time="2022-03-15T11:00:00Z"' $GENESIS > tmp.json && mv tmp.json $GENESIS
+jq '.genesis_time="2022-03-16T11:00:00Z"' $GENESIS > tmp.json && mv tmp.json $GENESIS
 
 rm -rf mainnet
 git clone https://github.com/cosmos-gaminghub/mainnet.git
@@ -36,6 +34,7 @@ cd mainnet/accounts && jq -s add $(ls |grep .json) > ../../game.json
 cd ../../
 
 echo 'importing snaphot.json and game.json...'
+gzip -d snapshot.json.gz
 nibirud import-genesis-accounts-from-snapshot ./snapshot.json ./game.json
 
 cp $GENESIS ./pre-genesis.json
